65eb56bd8f49f014e06662cf9157d1f5fe3cf5a2
Marco Mangan <marco.mangan@pucrs.br>
2015-11-25 21:36:59 -0200

    Atualizacao

--- src/br/pucrs/alpro3np/grafos/Graph.java
+++ src/br/pucrs/alpro3np/grafos/Graph.java
@@ -1,5 +1,11 @@
 package br.pucrs.alpro3np.grafos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
 
 	private int m[][];
@@ -10,6 +16,30 @@ public class Graph {
 		m = new int[nodes + 1][nodes + 1];
 	}
 
+	// TODO: ler arquivos do graphviz
+	// TODO: ler json, xml
+	public Graph(String filename) throws IOException {
+		FileReader fr = new FileReader(filename);
+		BufferedReader br = new BufferedReader(fr);
+
+		String n = br.readLine();
+		System.out.println(n);
+		int nodes = Integer.parseInt(n);
+		m = new int[nodes + 1][nodes + 1];
+
+		String pair;
+		while ((pair = br.readLine()) != null) {
+			System.out.println(pair);
+			String fields[] = pair.split(" ");
+			int a = Integer.parseInt(fields[0]);
+			int b = Integer.parseInt(fields[1]);
+			addEdge(a, b);
+		}
+
+		br.close();
+		fr.close();
+	}
+
 	public void addEdge(int a, int b) {
 		addEdge(a, b, 1);
 	}
@@ -18,18 +48,67 @@ public class Graph {
 		checkNode(a);
 		checkNode(b);
 		m[a][b] = v;
+		//m[b][a] = v;
+	}
+
+	public List<Integer> getAdjacents(int a) {
+		checkNode(a);
+		List<Integer> adjs = new ArrayList<>();
+
+		for (int i = 1; i < m.length; i++) {
+			if (m[a][i] != 0)
+				adjs.add(i);
+		}
+
+		return adjs;
 	}
 
 	private void checkNode(int a) {
 		if (a <= 0 || a > m.length)
 			throw new IllegalArgumentException("Invalid node: " + a);
+	}
 
+	/**
+	 * Profundidade
+	 * 
+	 * @return
+	 */
+
+	private boolean marked[];
+	
+	public List<Integer> depth(int a) {
+		checkNode(a);
+		List<Integer> nodes = new ArrayList<>();
+		marked = new boolean[m.length];
+		depth0(a, nodes);
+		return nodes;
+	}
+
+	private void depth0(int a, List<Integer> nodes) {
+		nodes.add(a);
+		marked[a] = true;
+		for (Integer adj : getAdjacents(a)) {
+			//if (!nodes.contains(adj))
+			if (! marked[adj])
+				depth0(adj, nodes);
+		}
+	}
+
+	/**
+	 * Largura
+	 * 
+	 * @return
+	 */
+	public List<Integer> breadth() {
+		List<Integer> nodes = new ArrayList<>();
+		// TODO:
+		return nodes;
 	}
 
 	@Override
 	public String toString() {
 		String s = "";
-		s += "    ";
+		s += "   ";
 		for (int i = 1; i < m.length; i++) {
 			s += i + " ";
 		}
@@ -43,6 +122,10 @@ public class Graph {
 			s += "\n";
 		}
 		return s;
+	}
+
+	public int getNodes() {
+		return m.length;
 	}
 
 	public void dijkstra() {
--- src/br/pucrs/alpro3np/grafos/GraphApp.java
+++ src/br/pucrs/alpro3np/grafos/GraphApp.java
@@ -1,10 +1,17 @@
 package br.pucrs.alpro3np.grafos;
 
+import java.io.IOException;
+
 public class GraphApp {
-	public static void main(String[] args) {
-		Graph g1 = new Graph(5);
+	public static void main(String[] args) throws IOException {
+		Graph g1 = new Graph("g1.txt");
+		// System.out.println(g1);
+		// g1.addEdge(1, 4);
+		// g1.addEdge(1, 5);
 		System.out.println(g1);
-		g1.addEdge(1, 5);
-		System.out.println(g1);
+		for (int i = 1; i < g1.getNodes(); i++)
+			System.out.println(i + " : " + g1.getAdjacents(i));
+		
+		System.out.println(g1.depth(2));
 	}
 }
